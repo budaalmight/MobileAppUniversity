781a2586327d5dca6cb8ef11aeddf26f342cb69e
kancho <kanchokanev7@gmail.com>
2016-06-26 21:48:19 +0300

    adding more images for the Places

--- app/src/main/java/user/mobileappuni/DatabaseHelper.java
+++ app/src/main/java/user/mobileappuni/DatabaseHelper.java
@@ -7,16 +7,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
-        super(context, "OurSuperCoolDB", null, 1);
+        super(context, "OurSuperCoolDB", null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS USER (USERNAME TEXT , PASSWORD TEXT , TOKEN TEXT , LAST TEXT)");
-        db.execSQL("INSERT INTO USER(USERNAME , PASSWORD) VALUE('a', 'a')");
-        db.execSQL("INSERT INTO USER(USERNAME , PASSWORD) VALUE('user@test.com', 'password')");
+        db.execSQL("INSERT INTO USER(USERNAME , PASSWORD) VALUES('a', 'a')");
+        db.execSQL("INSERT INTO USER(USERNAME , PASSWORD) VALUES('user@test.com', 'password')");
         db.execSQL("CREATE TABLE IF NOT EXISTS PLACES (NAME TEXT , PIC TEXT , DESCRIPTION TEXT, SPORT TEXT)");
-        db.execSQL("INSERT INTO PLACES(NAME,PIC,DESCRIPTION,SPORT) VALUES('Pulse','boxing_gym','Tuk ste moderni','Boxing'),('Boxi Boxi','boxing_gym','Opravqme zubi','Boxing'),('Pluv Pluv','swimming','Suprotivlenie pod nashata voda = 0','Swimming'),('Delfin4eto','swimming','Pluvane v salata','Swimming')");
+        db.execSQL("INSERT INTO PLACES(NAME,PIC,DESCRIPTION,SPORT) VALUES('Pulse','boxing_gym1','Tuk ste moderni','Boxing'),('Boxi Boxi','boxing_gym2','Opravqme zubi','Boxing'),('Pluv Pluv','swimming1','Suprotivlenie pod nashata voda = 0','Swimming'),('Delfin4eto','swimming2','Pluvane v salata','Swimming')");
 
     }
 
